d85b99cc4fe42e4b651f2851b825c7f7f0686336
Naveen Kewalramani <naveen@Mac.fritz.box>
2026-04-19 19:00:48 +0200

    V2

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/taxsimulator/',
+  base: '/tax-calculator/',
   plugins: [react()],
   server: { port: 3000 },
   test: {
